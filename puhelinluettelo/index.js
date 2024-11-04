904ed792e65eb3dd7c97b4dc4f20bce7784ec164
mmnuppoz <mmnuppoz@student.jyu.fi>
2024-11-04 13:23:36 +0200

    tehtävä 3.7 & 3.8

--- puhelinluettelo/index.js
+++ puhelinluettelo/index.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const app = express()
+var morgan = require('morgan')
+morgan.token('body', (req) => JSON.stringify(req.body))
 
 app.use(express.json())
+app.use(
+  morgan(':method :url :status :res[content-length] — :response-time ms :body')
+)
 
 let persons = [
     {
